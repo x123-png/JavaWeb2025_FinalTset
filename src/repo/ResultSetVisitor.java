640366beda9156f5805eccacacbcc2632e1a74de
x <2524517936@qq.com>
2025-12-11 14:39:29 +0800

    删除废弃文件

--- src/repo/ResultSetVisitor.java
+++
@@ -1,9 +0,0 @@
-package repo;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-public interface ResultSetVisitor<T> {
-    public List<T> visit(ResultSet rs) throws SQLException;
-}
